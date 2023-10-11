a560b9e98ae831c0abf3ce5847c5c79535809feb
Mikoyzskie <rapidstrike13@gmail.com>
2023-10-11 16:01:40 +0800

    end of day commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ const navigation = [
   { name: "Product", href: "#" },
   { name: "Gallery", href: "#" },
   { name: "Marketplace", href: "#" },
-  { name: "404", href: "/404" },
+  { name: "404", href: "/test" },
 ];
 
 export default function Header() {
@@ -37,7 +37,7 @@ export default function Header() {
         <div className="flex lg:hidden">
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
@@ -74,7 +74,7 @@ export default function Header() {
               <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src="https://res.cloudinary.com/dhavkybgu/image/upload/v1697006905/tjyaeqa3kwdqgxdrekmk.svg"
                 alt=""
               />
             </a>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
         ```
       */}
             <main className="not-found bg-[url('/hero.jpg')] bg-cover bg-fixed">
-                <div className="bg-black bg-opacity-50 w-full h-full px-6 py-24 sm:py-32 lg:px-8 grid place-items-center">
+                <div className="bg-black bg-opacity-50 w-full h-full px-6 py-40 sm:py-32 lg:px-8 grid place-items-center">
                     <div className="text-center">
                         <p className="text-base font-semibold text-white">404</p>
                         <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
@@ -20,7 +20,7 @@ export default function NotFound() {
                         <div className="mt-10 flex items-center justify-center gap-x-6">
                             <Link
                                 href="/"
-                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="rounded-md bg-[#d0a02b] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
                                 Go back home
                             </Link>
